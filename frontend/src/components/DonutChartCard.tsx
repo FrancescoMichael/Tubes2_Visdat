3f886ff87d238783421715c36c2e25f294809af2
RiciTrisnaP <ricitrisnap@gmail.com>
2025-05-27 00:32:15 +0700

    feat: layout

---
+++ frontend/src/components/DonutChartCard.tsx
@@ -0,0 +1,36 @@
+// src/components/DonutChartCard.tsx
+import { Doughnut } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import type { ChartData, ChartOptions } from 'chart.js'
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+interface DonutChartCardProps {
+  title: string
+  chartData: ChartData<'doughnut'>
+  chartOptions?: ChartOptions<'doughnut'>
+}
+
+const DonutChartCard: React.FC<DonutChartCardProps> = ({
+  title,
+  chartData,
+  chartOptions
+}) => {
+  return (
+    <div className="bg-white">
+      <div className='rounded-tr-2xl border-t-1 border-r-1 border-slate-300'>
+        <h2 className="text-2xl font-bold mb-4 pt-2 pl-2">{title}</h2>
+      </div>
+      <div className='p-4'>
+        <Doughnut data={chartData} options={chartOptions} />
+      </div>
+    </div>
+  )
+}
+
+export default DonutChartCard
