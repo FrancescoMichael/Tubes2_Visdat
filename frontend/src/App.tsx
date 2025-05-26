68412c4bf97c23ff2ddd6c0853a54c70b6b7052d
Francesco Michael Kusuma <francescokusuma15@gmail.com>
2025-05-26 19:30:05 +0700

    feat: setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello Vite + React + TypeScript + Tailwind!
+      </h1>
+    </div>
+  )
+}
+
+export default App
