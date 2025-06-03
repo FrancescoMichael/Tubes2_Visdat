37602cee46b722e323ff2984079cafb711c379b2
RiciTrisnaP <ricitrisnap@gmail.com>
2025-06-03 15:41:53 +0700

    feat: ready to deploy

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import os
+from flask import Flask,send_from_directory
 from flask_cors import CORS
 
 from config import Config
@@ -11,8 +12,23 @@ from app.routes.stats import stats_bp
 from app.routes.circuits import circuits_bp
 
 def create_app(config_class=Config):
-    app = Flask(__name__)
+    REACT_BUILD_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../frontend/dist'))
+    app = Flask(__name__, static_folder=REACT_BUILD_DIR, static_url_path='/')
     app.config.from_object(config_class)
+
+    # Tell Flask where to find the React build
+    @app.route('/')
+    def serve_react_app():
+        return send_from_directory(REACT_BUILD_DIR, 'index.html')
+    
+    @app.route('/<path:path>')
+    def serve_static(path):
+        full_path = os.path.join(REACT_BUILD_DIR, path)
+        if os.path.exists(full_path):
+            return send_from_directory(REACT_BUILD_DIR, path)
+        else:
+            # This handles React Router fallback
+            return send_from_directory(REACT_BUILD_DIR, 'index.html')
 
     db.init_app(app)
     CORS(app)
--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,8 @@
+from flask import send_from_directory
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+    
